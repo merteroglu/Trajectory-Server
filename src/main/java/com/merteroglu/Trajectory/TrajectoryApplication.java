5f3a971bf1b08c33bc6db75fb1ec11cc997696cc
Mert Erogu <merteroglu16@gmail.com>
2018-02-22 00:55:05 +0300

    First Commit
    Created Project

---
+++ src/main/java/com/merteroglu/Trajectory/TrajectoryApplication.java
@@ -0,0 +1,12 @@
+package com.merteroglu.Trajectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrajectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrajectoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/merteroglu/Trajectory/TrajectoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.merteroglu.Trajectory;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrajectoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
